76e175df1be9077863d034b5be37d03da39f6157
onlybranch <onlybranch@proton.me>
2022-06-22 06:56:43 +0000

    accidental commit

--- CSGuardian.cpp
+++ CSGuardian.cpp
@@ -1,4 +1,10 @@
 #include "stdafx.h"
 #include "CSGuardian.h"
 
-CCSGuardian::CCSGuardian(CRIT_SECT
+CCSGuardian::CCSGuardian(CRIT_SECTION &CS):
+	m_CS(CS)
+{
+#ifdef _WIN
+	m_CS.Lock();
+#elseif
+	m_CS.Stu
