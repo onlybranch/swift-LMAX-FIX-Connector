5174ea5e458feefd1bb34b85c9cd72cd69dcb6e0
onlybranch <onlybranch@proton.me>
2023-05-17 18:41:55 +0000

    Remove console logging

--- FIXAdapter.h
+++ FIXAdapter.h
@@ -41,4 +41,5 @@ private:
 	virtual void onCreate( const SessionID& );
 
 	// These are all the message types we can get back from the server
-	void	onMessage(const FIX44::Hea
+	void	onMessage(const FIX44::Heartbeat &message, const FIX::SessionID&);
+	void	onMessage
