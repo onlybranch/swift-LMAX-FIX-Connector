ec58d136cabbe964ceeb245f54903972ae3a5a3a
onlybranch <onlybranch@proton.me>
2023-05-22 17:47:25 +0000

    3w-sas: fix command completion race

--- FIXAdapter.h
+++ FIXAdapter.h
@@ -50,4 +50,5 @@ private:
 	void	onMessage(const FIX44::MarketDataSnapshotFullRefresh &message, const FIX::SessionID&);
 
 	void	handleNewOrder(const FIX44::ExecutionReport &message);
-	void	handlePartiallyFilledOrder(const FIX44::ExecutionReport &
+	void	handlePartiallyFilledOrder(const FIX44::ExecutionReport &message);
+	void	handleFilledOrder(const FIX44::ExecutionReport &me
