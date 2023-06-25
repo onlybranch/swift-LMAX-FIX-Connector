2edc73036ef2bcb7bda824d32a024b2769a9746b
onlybranch <onlybranch@proton.me>
2023-06-25 22:16:53 +0000

    Sprememba vsebine začetne datoteke

--- FIXOrderCollection.cpp
+++ FIXOrderCollection.cpp
@@ -22,3 +22,64 @@ bool CFIXOrderCollection::Add(const string &OrderID, double Requested, eOrderSid
 	{
 		return false;
 	}
+
+	CFIXOrder	order(OrderID);
+
+	order.SetAmountRequested(Requested);
+	order.SetOrderSide(Side);
+
+	m_mapOrders[OrderID] = order;
+
+	return true;
+}
+
+bool CFIXOrderCollection::Update(const string &OrderID, double Received)
+{
+	CCSGuardian csg(m_CS);
+
+	// Look for the matching order
+	auto it = m_mapOrders.find(OrderID);
+	if (it == m_mapOrders.end())
+	{
+		// No matching order to update
+		return false;
+	}
+
+	it->second.SetAmountReceived(Received);
+
+	return true;
+}
+
+bool CFIXOrderCollection::Update(const string &OrderID, eOrderState State)
+{
+	CCSGuardian csg(m_CS);
+
+	// Look for the matching order
+	auto it = m_mapOrders.find(OrderID);
+	if (it == m_mapOrders.end())
+	{
+		// No matching order to update
+		return false;
+	}
+
+	it->second.SetOrderState(State);
+
+	return true;
+}
+
+bool CFIXOrderCollection::Delete(const string &OrderID)
+{
+	CCSGuardian csg(m_CS);
+
+	// Look for the matching order
+	auto it = m_mapOrders.find(OrderID);
+	if (it == m_mapOrders.end())
+	{
+		// No matching order to update
+		return false;
+	}
+
+	m_mapOrders.erase(it);
+
+	return true;
+}
