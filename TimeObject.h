4b9053967425a7dbcc202e3d997c08a0beaec118
onlybranch <onlybranch@proton.me>
2023-07-12 21:32:32 +0000

    removed old version

--- TimeObject.h
+++ TimeObject.h
@@ -6,3 +6,28 @@
 #include <sys/timeb.h>
 #include <time.h>
 #endif // _WIN32
+
+class CTimeObject
+{
+private:
+#ifdef _WIN32
+	SYSTEMTIME	m_st;
+#else
+	timeb		m_tb;
+	tm			m_tm;
+#endif // _WIN32
+
+public:
+	unsigned int			GetMillisecond();
+	unsigned int			GetSecond();
+	unsigned int			GetMinute();
+	unsigned int			GetHour();
+	unsigned int			GetDay();
+	unsigned int			GetMonth();
+	unsigned int			GetYear();
+	static	CTimeObject		Now();
+	void 					SetCurrentTime();
+	void					Print();
+	void					PrintDate();
+	void					PrintTime();
+};
