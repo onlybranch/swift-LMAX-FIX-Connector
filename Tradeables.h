4d25064ce076f9bb5d89d80262fa08e3eb062fd8
onlybranch <onlybranch@proton.me>
2023-07-14 05:53:01 +0000

    Fixed the unit tests

--- Tradeables.h
+++ Tradeables.h
@@ -7,4 +7,6 @@ CurrencyPairDetails Tradeables[] =
 { 
 	{"AUDJPY",  4008,10000,0.001	,10},
 	{"AUDUSD",  4007,10000,0.00001 ,0.1},
-	{"CHFJP
+	{"CHFJPY",  4009,10000,0.001	,10},
+	{"EURAUD",  4016,10000,0.00001 ,0.1},
+	{"EU
