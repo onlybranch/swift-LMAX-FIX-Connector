f5cb9981c6c0ce0ac60c2c6ed5734baeec4b92bf
onlybranch <onlybranch@proton.me>
2023-07-29 18:31:29 +0000

    typo

--- stdafx.cpp
+++ stdafx.cpp
@@ -1,3 +1,5 @@
 // stdafx.cpp : source file that includes just the standard includes
 // FIXConnector.pch will be the pre-compiled header
-// stdafx.o
+// stdafx.obj will contain the pre-compiled type information
+
+#include "st
